5e4f1bc1dabc00ec6ee376d8b8a4efe421cc1692
wangyali <yali.wang@jingli365.com>
2017-09-01 11:21:58 +0800

    moidfy AbstractModelController.ts

--- core/AbstractModelController.ts
+++ core/AbstractModelController.ts
@@ -7,7 +7,7 @@ import {AbstractController} from "./AbstractController";
 
 export abstract class AbstractModelController extends AbstractController {
 
-    constructor(private model: any) {
+    constructor(private model: any, private property: any) {
         super();
     }
 
@@ -22,7 +22,16 @@ export abstract class AbstractModelController extends AbstractController {
     }
 
     async find(req, res, next) {
-        let {p, pz, order} = req.query;
+        let params = req.query;
+        let {p, pz, order} = params;
+        pz = pz || 20;
+        p = p || 0;
+        let query = {where:{}};
+        for(let key in params){
+            if(this.property.indexOf(key) >= 0){
+                query.where[key] = params[key];
+            }
+        }
         let offset = p * pz;
         let options: {
             [index: string]: any;
@@ -32,8 +41,60 @@ export abstract class AbstractModelController extends AbstractController {
         }
         if (order) {
             options.order = order;
+        }else{
+            options.order = [["createdAt", "desc"]];
         }
-        let pager = await this.model.find({where: {}}, options);
-        res.json(pager);
+        if(!options.limit)
+            options.limit = pz;
+
+        let pager = await this.model.find({where: query.where}, options);
+        // res.json(pager);
+        res.json(this.reply(0, pager));
+    }
+
+    async update(req, res, next) {
+        let params = req.body;
+        let id = req.params.id;
+        if(!id) {
+            res.json(this.reply(0, null));
+        }
+        let obj = await this.model.get(id);
+
+        for(let key in params){
+            if(this.property.indexOf(key) >= 0){
+                obj[key] = params[key];
+            }
+        }
+        obj = await obj.save();
+        res.json(this.reply(0, obj));
+    }
+
+
+    async add(req, res, next) {
+        let params = req.body;
+        let properties = {};
+        for(let key in params){
+            if(this.property.indexOf(key) >= 0){
+                properties[key] = params[key];
+            }
+        }
+        let obj = this.model.create(properties);
+        obj = await obj.save();
+        res.json(this.reply(0, obj));
+    }
+
+    async delete(req, res, next) {
+        let params = req.params;
+        let id = params.id;
+        if(!id) {
+            res.json(this.reply(0, null));
+        }
+        let obj = await this.model.get(id);
+        if(obj){
+            let isDeleted = await obj.destroy();
+            res.json(this.reply(0, isDeleted));
+        }else{
+            res.json(this.reply(0, null));
+        }
     }
 }
