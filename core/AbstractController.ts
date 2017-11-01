e30126ca5b6e58430df00471904bdcdfbea49087
wanglihui <lihui.wang@tulingdao.com>
2017-11-01 16:36:18 +0800

    modify error

--- core/AbstractController.ts
+++ core/AbstractController.ts
@@ -2,16 +2,11 @@
  * Created by wlh on 2017/8/29.
  */
 
+
 'use strict';
-import {IController} from "./IController";
+import {IController, ReplyData} from "./IController";
 
 import {ERR_TEXT} from './code';
-
-export abstract class AbstractController implements IController {
-
-    abstract $isValidId(id: string) :boolean;
-    reply: reply;
-}
 
 export function reply(code, data): ReplyData {
     let msg = ERR_TEXT[code];
@@ -21,3 +16,10 @@ export function reply(code, data): ReplyData {
         data,
     }
 }
+
+export abstract class AbstractController implements IController {
+
+    abstract $isValidId(id: string) :boolean;
+    reply= reply;
+}
+
